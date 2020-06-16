bca38d762efe0fbe034a97cbeb038aa36c7797ce
KevinPercy <kevinpercycl@gmail.com>
2020-06-16 16:30:55 -0500

    reverted axios to http

--- src/axios-blog.js
+++ src/axios-blog.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'https://ec2-18-191-233-225.us-east-2.compute.amazonaws.com/api/'
+    baseURL: 'http://ec2-18-191-233-225.us-east-2.compute.amazonaws.com/api/'
     // baseURL: 'http://127.0.0.1:8001/api/'
 });
 
