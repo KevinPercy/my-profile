006ba07fd646dea4784ee265b3996272d9a8af8b
KevinPercy <kevinpercycl@gmail.com>
2020-06-16 16:27:03 -0500

    added https to axios

--- src/axios-blog.js
+++ src/axios-blog.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'http://ec2-18-191-233-225.us-east-2.compute.amazonaws.com/api/'
+    baseURL: 'https://ec2-18-191-233-225.us-east-2.compute.amazonaws.com/api/'
+    // baseURL: 'http://127.0.0.1:8001/api/'
 });
 
 export default instance;
