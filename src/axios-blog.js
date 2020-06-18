738c2e3662f6654bbbaa0e3da300ae5bb895a8fb
KevinPercy <kevinpercycl@gmail.com>
2020-06-18 10:24:18 -0500

    added api url

--- src/axios-blog.js
+++ src/axios-blog.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'http://ec2-18-218-192-61.us-east-2.compute.amazonaws.com/api/'
+    baseURL: 'https://api.kevinccapatinta.com/api/'
     // baseURL: 'http://127.0.0.1:8001/api/'
 });
 
