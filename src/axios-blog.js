63a18c9b4249833222c24776451c3e5d1f34b62a
KevinPercy <kevinpercycl@gmail.com>
2020-06-18 17:59:43 -0500

    changed url from production

--- src/axios-blog.js
+++ src/axios-blog.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    // baseURL: 'https://api.kevinccapatinta.com/api/'
-    baseURL: 'http://127.0.0.1:8001/api/'
+    baseURL: 'https://api.kevinccapatinta.com/api/'
+    // baseURL: 'http://127.0.0.1:8001/api/'
 });
 
 export default instance;
