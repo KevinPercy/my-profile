d0a615e48ce26bca2d6619aa04f8a95670ff3999
KevinPercy <kevinpercycl@gmail.com>
2020-06-19 11:16:32 -0500

    Added ga(google analytics) library

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
 import { Route, Switch, withRouter } from 'react-router-dom';
+import ReactGA from 'react-ga';
 
 import Layout from './hoc/Layout/Layout';
 import Experiments from './containers/Blog/Experiments/Experiments';
 import Posts from './containers/Blog/Posts/Posts';
 import FullPost from './containers/Blog/Posts/FullPost';
+
+function initializeReactGA() {
+  ReactGA.initialize('UA-165333455-1');
+  ReactGA.pageview('/posts');
+}
 
 class App extends Component {
   render() {
